e1b0361f02bfe718a2e9a88396dfbb8ab02d7c2b
Juan Martínez Morató <juan.martinez.morato@gmail.com>
2026-09-10 23:19:27 +0100

    Remove legacy Python implementation from web MVP

--- src/threshold.py
+++
@@ -1,8 +0,0 @@
-class Threshold:
-    def __init__(self, number):
-        try:
-            self.value = int(number)
-            if self.value < 0:
-                raise ValueError()
-        except ValueError:
-            raise ValueError('Threshold initialized with invalid value: {}'.format(number))
